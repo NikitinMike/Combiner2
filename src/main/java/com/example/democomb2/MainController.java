2b416e54a747c6268be17dfc909d0eb3641209e2
mike <jobles11@gmail.com>
2022-12-15 11:39:57 +0300

    combine web page

--- src/main/java/com/example/democomb2/MainController.java
+++ src/main/java/com/example/democomb2/MainController.java
@@ -11,15 +11,48 @@ import java.util.List;
 @Controller
 public class MainController {
 
+    String[] sobaka = {"у_попа была собака",
+            "Он ее любил",
+            "Она съела кусок мяса",
+            "Он ее убил",
+            "И в землю закопал",
+            "И надпись написал",
+            "у_попа была собака",
+            "Он ее любил"};
+
+    String[] vorona = {"Шел я как-то через_мост",
+            "Глядь ворона сохнет",
+            "Взял ворону я за_хвост",
+            "Положил ее под_мост",
+            "Пусть ворона мокнет",
+            "Снова шел я через_мост",
+            "Глядь ворона мокнет",
+            "Снова взял ее за_хвост",
+            "Положил ее на_мост",
+            "Пусть ворона сохнет"};
+
+    String[] chuchelo = {"Чучело-мяучело на_трубе сидело",
+            "Чучело-мяучело песенку запело",
+            "Только всем от песенки этой стало тошно",
+            "Потому что песенка чучела про то что",
+            "Чучело мяучело на_трубе сидело"};
+    Combiner data = new Combiner("вихри враждебные веют над_нами");
+
+    @ModelAttribute("title")
+    public String title() {
+        return "COMBINER:" + data.words.length + "/" + data.amount;
+    }
+
     @ModelAttribute("messages")
     public List<String> messages() {
-//        return new Combiner("вихри враждебные веют над_нами").fullOut();
-        return new Combiner("у попа была собака").fullOut();
+        return data.fullOut();
     }
 
     @GetMapping("/")
     @ResponseBody
     public ModelAndView startPageGet() {
+        String s = vorona[(int) (vorona.length * Math.random())];
+        data = new Combiner(s);
         return new ModelAndView("page");
     }
 }
