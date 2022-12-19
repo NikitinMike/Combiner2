212d5e92cde058d86cb10813643328c35824f4e3
mike <jobles11@gmail.com>
2022-12-19 16:57:51 +0300

    find words in repository

--- src/main/java/com/example/democomb2/WordsRepository.java
+++ src/main/java/com/example/democomb2/WordsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WordsRepository extends CrudRepository<WordsEntity, Long> {
-
+    WordsEntity findByWordIgnoreCase(String word);
 }
