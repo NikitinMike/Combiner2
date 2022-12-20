2b987320ad476787e8b4e50c4b866e6e8548e2ba
mike <jobles11@gmail.com>
2022-12-20 13:59:07 +0300

    update working words list from DB with model attribute and word toString

--- src/main/java/com/example/democomb2/MainController.java
+++ src/main/java/com/example/democomb2/MainController.java
@@ -1,14 +1,13 @@
 package com.example.democomb2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController extends DataStreams {
@@ -21,30 +20,42 @@ public class MainController extends DataStreams {
 
     Combiner data = new Combiner("вихри враждебные веют над_нами");
 
-//    String[] words; // слова
-//    int[] parts; // какой части речи принадлежит
+    String[] words; // слова
+    int[] parts; // какой части речи принадлежит
 
-    @ModelAttribute("title")
-    public String title() {
-        return "COMBINER:" + data.words.length + "/" + data.amount;
-    }
+//    @ModelAttribute("title")
+//    public String title() {
+//        return "COMBINER:" + data.words.length + "/" + data.amount;
+//    }
 
-    @ModelAttribute("messages")
-    public List<String> messages() {
-        return data.fullOut();
-    }
+//    @ModelAttribute("messages")
+//    public List<String> messages() {
+//        return data.fullOut();
+//    }
 
     @GetMapping("/")
     @ResponseBody
-    public ModelAndView startPageGet() {
-        String s = vorona[(int) (vorona.length * Math.random())];
-        data = new Combiner(s);
+    public ModelAndView startPageGet(Model model) {
+//        String s = vorona[(int) (vorona.length * Math.random())];
+//        String s = sobaka[(int) (sobaka.length * Math.random())];
+        String s = chuchelo[(int) (chuchelo.length * Math.random())];
+        System.out.println("----------------------------------");
         System.out.println(s);
+        data = new Combiner(s);
+        model.addAttribute("title","COMBINER:" + data.words.length + "/" + data.amount);
         for (WordsEntity word : getWords(data.getWords())) System.out.println(word);
+        model.addAttribute("messages", data.fullOut());
         return new ModelAndView("page");
     }
 
     private List<WordsEntity> getWords(String[] words) {
-        return Arrays.stream(words).flatMap(word -> repository.findAllByWord(word).stream()).collect(Collectors.toList());
+//        return Arrays.stream(words).flatMap(word -> repository.findAllByWord(word).stream()).collect(Collectors.toList());
+        List<WordsEntity> wordsEntityList = new ArrayList<>();
+        for (String word : words) {
+            String [] subWords = word.split("_");
+            if(subWords.length>1) wordsEntityList.addAll(getWords(subWords));
+            else wordsEntityList.addAll(repository.findAllByWord(word));
+        }
+        return wordsEntityList;
     }
 }
--- src/main/java/com/example/democomb2/WordsEntity.java
+++ src/main/java/com/example/democomb2/WordsEntity.java
@@ -3,14 +3,12 @@ package com.example.democomb2;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
 @Table(name = "words", schema = "words")
 public class WordsEntity {
@@ -21,21 +19,21 @@ public class WordsEntity {
     @Basic
     @Column(name = "word")
     private String word;
-//    @Basic
-//    @Column(name = "code")
-//    private long code;
-//    @Basic
-//    @Column(name = "code_parent")
-//    private long codeParent;
+    @Basic
+    @Column(name = "code")
+    private long code;
+    @Basic
+    @Column(name = "code_parent")
+    private long codeParent;
     @Basic
     @Column(name = "type")
     private String type;
-//    @Basic
-//    @Column(name = "type_sub")
-//    private String typeSub;
-//    @Basic
-//    @Column(name = "type_ssub")
-//    private String typeSsub;
+    @Basic
+    @Column(name = "type_sub")
+    private String typeSub;
+    @Basic
+    @Column(name = "type_ssub")
+    private String typeSsub;
     @Basic
     @Column(name = "plural")
     private Byte plural;
@@ -45,9 +43,9 @@ public class WordsEntity {
     @Basic
     @Column(name = "wcase")
     private String wcase;
-//    @Basic
-//    @Column(name = "comp")
-//    private String comp;
+    @Basic
+    @Column(name = "comp")
+    private String comp;
     @Basic
     @Column(name = "soul")
     private Byte soul;
@@ -57,22 +55,63 @@ public class WordsEntity {
     @Basic
     @Column(name = "perfect")
     private Byte perfect;
-//    @Basic
-//    @Column(name = "face")
-//    private String face;
-//    @Basic
-//    @Column(name = "kind")
-//    private String kind;
+    @Basic
+    @Column(name = "face")
+    private String face;
+    @Basic
+    @Column(name = "kind")
+    private String kind;
     @Basic
     @Column(name = "time")
     private String time;
-//    @Basic
-//    @Column(name = "inf")
-//    private Byte inf;
-//    @Basic
-//    @Column(name = "vozv")
-//    private Byte vozv;
-//    @Basic
-//    @Column(name = "nakl")
-//    private String nakl;
+    @Basic
+    @Column(name = "inf")
+    private Byte inf;
+    @Basic
+    @Column(name = "vozv")
+    private Byte vozv;
+    @Basic
+    @Column(name = "nakl")
+    private String nakl;
+
+    String notNull(String tag, String s) {
+        if (s == null) return "";
+        return String.format(", %s='%s'", tag, s);
+    }
+
+    String notNull(String tag, Byte b) {
+        if (b == null || 0 == b) return "";
+        return String.format(", %s=%d", tag, b);
+    }
+
+    @Override
+    public String toString() {
+        return word
+//                + "iid=" + iid
+//                + ", word='" + word + '\''
+//                + ", code=" + code
+//                + notNull("codeParent", codeParent)
+                + "\t" + type
+                + notNull("typeSub", typeSub)
+                + notNull("typeSsub", typeSsub)
+                + notNull("plural", plural)
+                + notNull("gender", gender)
+                + notNull("wcase", wcase)
+                + notNull("comp", comp)
+                + notNull("soul", soul)
+                + notNull("transit", transit)
+                + notNull("perfect", perfect)
+                + notNull("face", face)
+                + notNull("kind", kind)
+                + notNull("time", time)
+                + notNull("inf", inf)
+                + notNull("vozv", vozv)
+                + notNull("nakl", nakl)
+                ;
+    }
+
+    private String notNull(String tag, Long c) {
+        if (c == null || 0 == c) return "";
+        return String.format(", %s=%d", tag, c);
+    }
 }
