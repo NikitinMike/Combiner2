da5c5793a6ad28e7282ee1fa532a522290d99967
mike <jobles11@gmail.com>
2022-12-20 10:11:46 +0300

    find words in repository indexed

--- src/main/java/com/example/democomb2/WordsRepository.java
+++ src/main/java/com/example/democomb2/WordsRepository.java
@@ -3,7 +3,9 @@ package com.example.democomb2;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WordsRepository extends CrudRepository<WordsEntity, Long> {
-    WordsEntity findByWord(String word);
+    List<WordsEntity> findAllByWord(String word);
 }
